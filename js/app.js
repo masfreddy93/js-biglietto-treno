60aed1f9ccef49daac8f488936c54e9409b507f6
masfreddy93 <masieronicola@gmail.com>
2022-07-20 15:03:09 +0200

    edit js: price without discount

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('ciao belo')
+const kmDaPercorrere = parseInt(prompt('Inserisci il numero di km che vuoi percorrere, per favore'));
+const etaPasseggero = parseInt(prompt('Inserisci la tua età, per favore'));
+console.log(kmDaPercorrere, etaPasseggero);
+
+const prezzoAlKm = 0.21;
+const prezzoBiglietto = prezzoAlKm * kmDaPercorrere;
+console.log('Il prezzo del biglietto - al netto di sconti - è il seguente: ' + prezzoBiglietto + ' Є');
+
